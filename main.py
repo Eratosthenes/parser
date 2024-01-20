7644d0a891d0ef456b13e1f2835007cbfa588e0a
Nathan Rapport <n.rapport@gmail.com>
2024-01-20 13:45:51 -0500

    reorganizing + fleshing out language tokens

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
 import re
+import sys
+from lexer.lexer import TokenTable, Lexer
 
 def read_tokens(filename):
     """ returns: TokenTable """
@@ -44,104 +45,6 @@ def make_partial_tokens(tok_rx, fixed_tokens):
         pattern = re.compile(r'^%s$' % re.escape(partial_tok_rx))
         fixed_tokens[partial_tok_rx] = "ERROR", pattern
 
-class Token:
-    def __init__(self, type, value):
-        self.type = type
-        self.value = value
-    
-    def __repr__(self):
-        return f"<{self.type}: {repr(self.value)}>"
-
-class TokenTable:
-    def __init__(self, fixed_tokens, variable_tokens):
-        self.fixed_tokens = fixed_tokens
-        self.variable_tokens = variable_tokens
-
-class Lexer:
-    def __init__(self, token_table):
-        self.i = 0
-        self.j = -1 # peek index
-        self.buf = []
-        self.fixed_tokens = token_table.fixed_tokens
-        self.variable_tokens = token_table.variable_tokens
-
-    def _flush(self):
-        self.buf = []
-        self.j = self.i-1
-    
-    def _is_match(self):
-        test_str = ''.join(self.buf)
-
-        # try fixed_tokens
-        if self.fixed_tokens.get(test_str) != None:
-            _, pattern = self.fixed_tokens[test_str]
-            return pattern, True
-
-        # try variable_tokens
-        for pattern in self.variable_tokens.keys():
-            pmatch = pattern.match(test_str)
-            if pmatch and pmatch.end() == pmatch.endpos:
-                return pattern, True
-
-        return None, False
-
-    def _peek(self):
-        self.j += 1
-        if self.j == len(self.text):
-            return False
-        self.buf.append(self.text[self.j])
-        return True
-    
-    def set(self, text):
-        """ set text to tokenize and reset internal state """
-        self.text = text
-        self.i = 0
-        self._flush()
-        return self
-
-    def next(self):
-        if not self._peek():
-            return
-
-        prev_pattern, found = self._is_match()
-        if not found:
-            raise Exception(self.text[self.i:].partition(" ")[0])
-
-        while True:
-            pattern, found = self._is_match()
-            if not found:
-                break
-            else:
-                prev_pattern = pattern
-                if not self._peek():
-                    break
-        
-        token_value = self.text[self.i:self.j]
-        if self.fixed_tokens.get(token_value) != None:
-            token_type, _ = self.fixed_tokens[token_value]
-        else:
-            token_type = self.variable_tokens[prev_pattern]
-
-        self.i += self.j - self.i
-        self._flush()
-        return Token(token_type, token_value)
-
-    def lex(self):
-        """ return tokens """
-        tokens = []
-        while True:
-            try:
-                token = self.next()
-                if not token:
-                    break
-                if token.type not in {"SPACE", "NEWLINE"}:
-                    tokens.append(token)
-            except Exception as e:
-                tokens.append(Token("ERROR", str(e)))
-                break
-
-        return tokens
-
 def repl(lexer):
     print("Enter an expression ('q' to quit):")
     while True:
@@ -177,7 +80,7 @@ test_cases = [
 def main():
     # lexer.py
     if len(sys.argv) == 1: 
-        repl(Lexer(read_tokens('bnf.tok')))
+        repl(Lexer(read_tokens('language.tok')))
 
     # lexer.py test
     elif "test" in sys.argv[1]: 
