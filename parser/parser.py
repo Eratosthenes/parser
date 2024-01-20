dd1b5fdb833ce04df61196a689d45198b54a4cd6
Nathan Rapport <n.rapport@gmail.com>
2024-01-20 18:55:39 -0500

    using class variables for state

--- parser/parser.py
+++ parser/parser.py
@@ -20,60 +20,62 @@ def parse(lexer: Lexer, tokens: List[Token]):
         print(rule)
 
 class Rule:
-    # elements are either a terminal or another rule
-    def __init__(self, lhs, elements=[]):
+    def __init__(self, lhs):
         self.lhs = lhs
-        self.elements = elements 
+        self.rules = [] 
     
     def __repr__(self):
-        return f"<{self.lhs}: {self.elements}>"
+        return f"<{self.lhs}: {self.rules}>"
     
     def add(self, element):
-        self.elements.append(element)
+        self.rules.append(element)
     
     def reduce(self):
-        rule = Rule(self.lhs, [])
-        new_elements = []
-        for element in self.elements:
-            if type(element) == str:
-                rule.add(element)
+        rule = Rule(self.lhs)
+        new_rules = []
+        for r in self.rules:
+            if type(r) == str:
+                rule.add(r)
             else:
-                new_elements.append(element)
+                new_rules.append(r)
         
-        new_elements.append(rule)
-        self.elements = new_elements
+        new_rules.append(rule)
+        self.rules = new_rules
 
 
 class StateMachine:
-    def __init__(self, state="SET_LHS"):
+    SET_LHS = "SET_LHS"
+    ADD_ELEMENT = "ADD_ELEMENT"
+
+    def __init__(self, state=SET_LHS):
         self.state = state
-        self.token_stack = []
 
     def next(self, token: Token):
         if token.type == "BNF_NAME":
 
-            if self.state == "SET_LHS":
-                self.rule = Rule(token.value, [])
+            if self.state == self.SET_LHS:
+                self.rule = Rule(token.value)
                 return
 
-            elif self.state == "ADD_ELEMENT":
+            elif self.state == self.ADD_ELEMENT:
                 self.rule.add(token.value)
                 return
 
         elif token.type == "COLON":
-            self.state = "ADD_ELEMENT"
+            self.state = self.ADD_ELEMENT
             return
         
         elif token.type == "TERMINAL":
-            self.state = "ADD_ELEMENT"
+            self.state = self.ADD_ELEMENT
             self.rule.add(token.value)
+            return
 
         elif token.type == "PIPE":
             self.rule.reduce()
-            self.state = "ADD_ELEMENT"
+            self.state = self.ADD_ELEMENT
             return
 
         elif token.type == "SEMICOLON":
             self.rule.reduce()
-            self.state = "SET_LHS"
+            self.state = self.SET_LHS
             return self.rule
